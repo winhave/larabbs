999d549df3395ce21a010babb665f4049f757f2a
Jun.Lee <jun.lee@qq.com>
2019-03-14 22:01:48 +0800

    站点权限部署

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,6 +27,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        \Horizon::auth(function ($request) {
+            // 是否站长
+            return \Auth::user()->hasRole('Founder');
+        });
     }
 }
